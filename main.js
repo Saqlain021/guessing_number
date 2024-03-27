ef4e9b2ac2c4774941b98af2f468172632d742da
saqlain <saqlainali.saqlainali08@gmail.com>
2024-03-27 15:25:02 +0500

    first time pushing

---
+++ main.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const random = Math.floor(Math.random() * 10 + 1);
+const a = await inquirer.prompt([{
+        message: "enter your guess number here : ",
+        type: "number",
+        name: "guessNumber"
+    }
+]);
+if (a.guessNumber === random) {
+    console.log("congrats! you guess a right number.");
+}
+else {
+    console.log("you guess a wrong number, please try again.");
+}
